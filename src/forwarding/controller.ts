56e5c5ab420266ace558031feedd81047f861446
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2018-05-02 17:36:21 +0200

    no more need for the Get function

--- src/forwarding/controller.ts
+++ src/forwarding/controller.ts
@@ -1,28 +1,31 @@
-// src/webhooks-url/controller.ts
-import { JsonController, Get, Param, Put, Body, NotFoundError,Post, HttpCode} from 'routing-controllers'
+// src/forwarding/controller.ts
+import { JsonController, Body,Post, HttpCode} from 'routing-controllers'
 import Forwarding from './entity'
-import { Timestamp } from 'typeorm';
-// import { Timestamp } from 'typeorm';
+import UrlTable from '../webhooks-url/entity'
 
+// import { Timestamp } from 'typeorm';
 
 @JsonController()
 
-
 export default class ForwardingController {
-
 
     @Post('/postquizresult')
     @HttpCode(200)
-    createUrl(
+    async createUrl(
     @Body() body: Forwarding) {
     
     const forwarding = new Forwarding()
-    forwarding.qid = body.qid
-    forwarding.qobject = body.qobject
-    forwarding.httpcode = body.httpcode
-    forwarding.lasttry = 0
-    forwarding.save()
-    return 'Ok'
+        forwarding.qid = body.qid
+        forwarding.qobject = body.qobject
+        // Now fetch the url from the other table.
+        const address = await UrlTable.findOne({qid: body.qid})
+        // Because we imported the entity we don't need to use @Get to find the ID.
+
+        forwarding.httpcode = body.httpcode
+        forwarding.lasttry = 0
+        forwarding.save()
+    return 'Thank you, bye bye...'
+
     }
 
 }
