5fd70bb936a0acb395933ea27efe4797b1559519
atharv7 <virtuarchitects7@gmail.com>
2018-05-04 10:11:10 +0200

    http codes fixed finally

--- src/forwarding/controller.ts
+++ src/forwarding/controller.ts
@@ -3,6 +3,20 @@ import { JsonController, Body,Post, HttpCode, NotFoundError } from 'routing-cont
 import Forwarding from './entity'
 import UrlTable from '../webhooks-url/entity'
 import * as request from 'superagent'
+const sendToUrl = (url,tosave) => {
+    request
+        .post(url)
+        .send(tosave.qobject)
+        .then((response)=> {
+            tosave.httpcode=response.status
+            tosave.save()
+        })
+        .catch((error: any) =>{
+            tosave.httpcode=error.status
+            tosave.save()
+        })
+}
+
 
 // import { Timestamp } from 'typeorm';
 
@@ -16,29 +30,28 @@ export default class ForwardingController {
     @Body() body: Forwarding) {
     
     const forwarding = new Forwarding()
+  
         forwarding.qid = body.qobject.quiz_id
         forwarding.qobject = body.qobject
-        console.log('forwarding.qobject: ', forwarding.qobject)
+        // console.log('forwarding.qobject: ', forwarding.qobject)
         // Now fetch the url from the other table.
         const address = await UrlTable.findOne({qid: forwarding.qid})
-
+        
         // Send the object we recieved to the address from UrlTable
         if(address){
-            request
-            .post(address.url)
-            .send(forwarding.qobject)
-            .end((err, res) => {
-                if(res)
-                console.log(res)
-                else
-                console.log(err)
-            })
+            await sendToUrl(address.url,forwarding)
+            // request
+            // .post(address.url)
+            // .send(forwarding.qobject)
+            // .end((err, res) => {
+            //     if(res)
+            //     console.log(res)
+            //     else
+            //     console.log(err)
+            // })
         }
-        else {console.log('error')}
-        forwarding.httpcode = body.httpcode
-        forwarding.lasttry = 0
-        forwarding.save()
-    return 'Great! It worked...'
+        else {return 'thankyou! but no webhook assigned to this quiz!'}
+        return 'kthnxbi :)'
 
     }
 
@@ -49,8 +62,8 @@ export default class ForwardingController {
     @Body() body: any) {
         console.log('testurl/body', body)
         const thouShallPass = (Math.floor(Math.random()*20)%2==0)?true:false
-        if (thouShallPass){
-            return 'Webhook success!'
+        if (thouShallPass===true){
+            return 'Thanks!'
         } else {
             throw new NotFoundError
         }
